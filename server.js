461344adcfe74ce6528e2f56112df46844e84ca5
Alabi Emmanuel <bobslegend795@gmail.com>
2019-03-03 04:39:37 +0100

    Added winston logger

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
+const logger = require('./logger');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`App running on port: ${ PORT }`));
+app.listen(PORT, () => logger.log({
+    level: 'info',
+    message: `App running on port: ${ PORT }`
+}));

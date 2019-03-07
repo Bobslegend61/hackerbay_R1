fdf872a924a1e6acc1df4afa2617099994e1f453
Alabi Emmanuel <bobslegend795@gmail.com>
2019-03-07 17:02:39 +0100

    done

---
+++ docs/script/search_index.js
@@ -0,0 +1,326 @@
+window.esdocSearchIndex = [
+  [
+    "round one/.external-ecmascript.js~array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array",
+    "Round one/.external-ecmascript.js~Array",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~arraybuffer",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer",
+    "Round one/.external-ecmascript.js~ArrayBuffer",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~boolean",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean",
+    "Round one/.external-ecmascript.js~Boolean",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~dataview",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DataView",
+    "Round one/.external-ecmascript.js~DataView",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~date",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date",
+    "Round one/.external-ecmascript.js~Date",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~error",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error",
+    "Round one/.external-ecmascript.js~Error",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~evalerror",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/EvalError",
+    "Round one/.external-ecmascript.js~EvalError",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~float32array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Float32Array",
+    "Round one/.external-ecmascript.js~Float32Array",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~float64array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Float64Array",
+    "Round one/.external-ecmascript.js~Float64Array",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~function",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function",
+    "Round one/.external-ecmascript.js~Function",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~generator",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Generator",
+    "Round one/.external-ecmascript.js~Generator",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~generatorfunction",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/GeneratorFunction",
+    "Round one/.external-ecmascript.js~GeneratorFunction",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~infinity",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Infinity",
+    "Round one/.external-ecmascript.js~Infinity",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~int16array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Int16Array",
+    "Round one/.external-ecmascript.js~Int16Array",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~int32array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Int32Array",
+    "Round one/.external-ecmascript.js~Int32Array",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~int8array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Int8Array",
+    "Round one/.external-ecmascript.js~Int8Array",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~internalerror",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/InternalError",
+    "Round one/.external-ecmascript.js~InternalError",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~json",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON",
+    "Round one/.external-ecmascript.js~JSON",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~map",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map",
+    "Round one/.external-ecmascript.js~Map",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~nan",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NaN",
+    "Round one/.external-ecmascript.js~NaN",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~number",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number",
+    "Round one/.external-ecmascript.js~Number",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~object",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object",
+    "Round one/.external-ecmascript.js~Object",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~promise",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise",
+    "Round one/.external-ecmascript.js~Promise",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~proxy",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy",
+    "Round one/.external-ecmascript.js~Proxy",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~rangeerror",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RangeError",
+    "Round one/.external-ecmascript.js~RangeError",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~referenceerror",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ReferenceError",
+    "Round one/.external-ecmascript.js~ReferenceError",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~reflect",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Reflect",
+    "Round one/.external-ecmascript.js~Reflect",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~regexp",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp",
+    "Round one/.external-ecmascript.js~RegExp",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~set",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set",
+    "Round one/.external-ecmascript.js~Set",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~string",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String",
+    "Round one/.external-ecmascript.js~String",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~symbol",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol",
+    "Round one/.external-ecmascript.js~Symbol",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~syntaxerror",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SyntaxError",
+    "Round one/.external-ecmascript.js~SyntaxError",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~typeerror",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypeError",
+    "Round one/.external-ecmascript.js~TypeError",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~urierror",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/URIError",
+    "Round one/.external-ecmascript.js~URIError",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~uint16array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint16Array",
+    "Round one/.external-ecmascript.js~Uint16Array",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~uint32array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint32Array",
+    "Round one/.external-ecmascript.js~Uint32Array",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~uint8array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array",
+    "Round one/.external-ecmascript.js~Uint8Array",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~uint8clampedarray",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8ClampedArray",
+    "Round one/.external-ecmascript.js~Uint8ClampedArray",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~weakmap",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WeakMap",
+    "Round one/.external-ecmascript.js~WeakMap",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~weakset",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WeakSet",
+    "Round one/.external-ecmascript.js~WeakSet",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~boolean",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean",
+    "Round one/.external-ecmascript.js~boolean",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~function",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function",
+    "Round one/.external-ecmascript.js~function",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~null",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/null",
+    "Round one/.external-ecmascript.js~null",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~number",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number",
+    "Round one/.external-ecmascript.js~number",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~object",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object",
+    "Round one/.external-ecmascript.js~object",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~string",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String",
+    "Round one/.external-ecmascript.js~string",
+    "external"
+  ],
+  [
+    "round one/.external-ecmascript.js~undefined",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/undefined",
+    "Round one/.external-ecmascript.js~undefined",
+    "external"
+  ],
+  [
+    "round one/auth.js",
+    "file/Round one/auth.js.html",
+    "Round one/auth.js",
+    "file"
+  ],
+  [
+    "round one/helperfunc.js",
+    "file/Round one/helperFunc.js.html",
+    "Round one/helperFunc.js",
+    "file"
+  ],
+  [
+    "round one/logger.js",
+    "file/Round one/logger.js.html",
+    "Round one/logger.js",
+    "file"
+  ],
+  [
+    "round one/router.js",
+    "file/Round one/router.js.html",
+    "Round one/router.js",
+    "file"
+  ],
+  [
+    "round one/server.js",
+    "file/Round one/server.js.html",
+    "Round one/server.js",
+    "file"
+  ],
+  [
+    "round one/test/helperfunc.js",
+    "file/Round one/test/helperFunc.js.html",
+    "Round one/test/helperFunc.js",
+    "file"
+  ],
+  [
+    "round one/test/router.js",
+    "file/Round one/test/router.js.html",
+    "Round one/test/router.js",
+    "file"
+  ]
+]
